f3e8c54106821f3079a075996acc8e3d3d27720d
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-15 22:05:45 -0500

    refactor:  :recycle: Code
    Add new things, besides removing code that was
    not relevant, and fix small conflicts

--- main.js
+++ main.js
@@ -1,94 +1,55 @@
-// const Campus = {
-//     Contact: {
-//         Mexico: "+52 648999",
-//         Colombia: {
-//             Bogota: "+57 3152365455",
-//             Medelliin: "+57 3145212325",
-//             Bucaramanga: "+57 3118807659"
-//         }
-//     },
-//     Trainers:[
-//         {name_trainer: "miguel", 
-//          phone: "3188121515",
-//          teams: 2,
-//          name_group: "Sputnik",
-//          salons: [1, 6],
-//          email: "example@name.com",
-//          schedules: ["6:00am", "2:00pm"]
-//         },
-//         {name_trainer: "Pepe", 
-//          phone: "5115132661",
-//          teams: 2,
-//          name_group: "Artemis",
-//          salons: [1, 6],
-//          email: "example@name.com",
-//          schedules: ["6:00am", "2:00pm"]
-//         },
-//         {name_trainer: "jolber", 
-//          phone: "5151545545",
-//          teams: 2,
-//          name_group: "Apolo",
-//          salons: [1, 6],
-//          email: "example@name.com",
-//          schedules: ["6:00am", "2:00pm"]
-//         }
-//     ],
-//     Campers: [
-//         {camper: "Miller",
-//          phone: "3188987644",
-//          team:{
-//             name_group: "Sputnik",
-//             group: "A1",
-//             schedules: ["6:00am", "2:00pm"],
-//             days: 5 + "for " + "Week",
-//             salons: [2, 6],
-//             email: "example@name.com",
-//             subjects: ["Web development", "English", "Be"]
-//          },
-//          level: {
-//             neighborhood: "Hamacas", 
-//             transport: "Bus",
-//             pre_requirement: "",
-//             technology: "",
-//         },
-//         roadMap: {
-//             credits: "#",
-//             year: 2023, 
-//             n_subjects: ""
-//         }
-//        }
-//     ]
-// }; 
-
-{/* <option value="trainer1" selected>Miguel</option>
-			<option value="trainer2">Bermer</option>
-			<option value="trainer3">Jolber</option> */}
-
 let Campus = {}
 let form_sede = document.querySelector("#form-campus"); 
-let form_info_members = document.querySelector("#form-trainer-camper"); 
+let form_info_members = document.querySelector("#form_usuarios"); 
 let sede_seleccionada = document.querySelector("[name='sede_seleccionada']"); 
+let tmp; 
+let object_union; 
 
 const addInfo = (event) => {
     event.preventDefault();
     let data = Object.fromEntries(new FormData(event.target));
-    if(data.sede in Campus) window.alert("Ya existe")
+    if(data.sede in Campus) Swal.fire({icon: "error",title: "Ya existe"})
     else {
         Campus[data.sede] = [];
         sede_seleccionada.insertAdjacentHTML("beforeend", `
-        <option value="${data.sede}">${data.sede}</option>
+        <option  value="${data.sede}">${data.sede}</option>
      `); 
     }
+    form_sede.reset(); 
 }
 
 const addPropertys = (event) => {
     event.preventDefault();
     let data = Object.fromEntries(new FormData(event.target));
     if(Campus[sede_seleccionada.value] != undefined) {
-        Object.entries(data).forEach(info => Campus[sede_seleccionada.value].unshift(info));
+        tmp = []; 
+        Object.entries(data).forEach(info => {
+            let atpm = Object.fromEntries([info]);
+            tmp.push(atpm); 
+        });
+        for(let x = 0; x < tmp.length; x++){
+            object_union = ({...tmp[x],...tmp[x+1],
+            ...tmp[x+2],...tmp[x+3],...tmp[x+4], 
+            ...tmp[x+5],...tmp[x+6],nivel:[{
+            ...tmp[x+7],...tmp[x+8],...tmp[x+9]}], 
+            Trainer:[{...tmp[x+10],...tmp[x+11],
+            ...tmp[x+12],...tmp[x+13]}], RoadMap:[
+                {...tmp[x+14],...tmp[x+15],...tmp[x+16],
+                ...tmp[x+17]
+            }]
+        });
+            break; 
+        }
     }
-    return console.log(Campus);  
+     Campus[sede_seleccionada.value].unshift(object_union); 
+     form_info_members.reset(); 
+     return (Swal.fire({
+        icon:'success',
+        title: "Envio realizado",
+        text: 'Los datos se enviaron de forma exitosa'
+     }), console.log(Campus));
 }
 
-form_sede.addEventListener("submit", e => addInfo(e))
+form_sede.addEventListener("submit", e => addInfo(e));
 form_info_members.addEventListener("submit", (e) => addPropertys(e)); 
+
