4ff79c98f608ed0d6db4bb273911cf3796d761da
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-03-16 20:51:31 -0500

    feat: :sparkles: Advancing with exercise
    Changed structure of code and add new features in to scope "AdInfo" and the object "Campus"

--- main.js
+++ main.js
@@ -1,69 +1,105 @@
-let Campus = {}
+let Campus = {sedes: {contacto:[]}};
 let form_sede = document.querySelector("#form-campus");
 let form_info_members = document.querySelector("#form_usuarios");
 let sede_seleccionada = document.querySelector("[name='sede_seleccionada']");
-let inputs = document.querySelectorAll(".check");
 let tmp;
 let object_union;
 
 const addInfo = (event) => {
-    event.preventDefault();
-    let data = Object.fromEntries(new FormData(event.target));
-    if (data.sede in Campus) Swal.fire({ icon: "error", title: "Ya existe" })
-    else if (data.sede != "") {
-        Campus[data.sede] = { integrantes: [] };
-        sede_seleccionada.insertAdjacentHTML("beforeend", `
+  event.preventDefault();
+  let data = Object.fromEntries(new FormData(event.target));
+  let sede = data.sede;
+  if (sede in Campus["sedes"]) Swal.fire({ icon: "error", title: "Ya existe" });
+  else if (sede != "") {
+    Campus["sedes"][sede] = { integrantes: [] };
+    let info_contacto = {
+        sede: sede,
+        contacto: data.contacto_sede,
+        direccion: data.direccion_sede,
+    };
+    Campus.sedes.contacto.unshift(info_contacto)
+    sede_seleccionada.insertAdjacentHTML(
+      "beforeend",
+      `
         <option  value="${data.sede}">${data.sede}</option>
-     `);
-    } else {
-        Swal.fire({
-            icon: "error",
-            title: "Campo vacio"
-        })
-    }
-    form_sede.reset();
-}
+     `
+    );
+  } else {
+    Swal.fire({
+      icon: "error",
+      title: "Campo vacio",
+    });
+  }
+  form_sede.reset();
+};
 
 const addPropertys = (event) => {
-    event.preventDefault();
-    let data = Object.fromEntries(new FormData(event.target));
-    if (Campus[sede_seleccionada.value] != undefined) {
-        tmp = [];
-        Object.entries(data).forEach(info => {
-            let atpm = Object.fromEntries([info]);
-            tmp.push(atpm);
-        });
-        for (let x = 0; x < tmp.length; x++) {
-            object_union = ({
-                Camper: [{
-                    ...tmp[x], ...tmp[x + 1],
-                    ...tmp[x + 2], ...tmp[x + 3], ...tmp[x + 4],
-                    ...tmp[x + 5], ...tmp[x + 6]}],
+  event.preventDefault();
+  let data = Object.fromEntries(new FormData(event.target));
+  if (Campus["sedes"][sede_seleccionada.value] != undefined) {
+    tmp = [];
+    Object.entries(data).forEach((info) => {
+      let atpm = Object.fromEntries([info]);
+      tmp.push(atpm);
+    });
+    for (let x = 0; x < tmp.length; x++) {
+      object_union = {
+        Camper: [
+          {
+            ...tmp[x],
+            ...tmp[x + 1],
+            ...tmp[x + 2],
+            ...tmp[x + 3],
+            ...tmp[x + 4],
+            ...tmp[x + 5],
+            ...tmp[x + 6],
+            ...tmp[x + 7],
+          },
+        ],
 
-                nivel: [{
-                    ...tmp[x + 7], ...tmp[x + 8], ...tmp[x + 9]}],
-                    
-                Trainer: [{
-                    ...tmp[x + 10], ...tmp[x + 11],
-                    ...tmp[x + 12], ...tmp[x + 13]}], 
+        nivel: [
+          {
+            ...tmp[x + 8],
+            ...tmp[x + 9],
+            ...tmp[x + 10],
+          },
+        ],
 
-                RoadMap: [{
-                    ...tmp[x + 14], ...tmp[x + 15], ...tmp[x + 16],
-                    ...tmp[x + 17]}]
-            });
-            break;
-        }
+        Trainer: [
+          {
+            ...tmp[x + 11],
+            ...tmp[x + 12],
+            ...tmp[x + 13],
+            ...tmp[x + 14],
+            ...tmp[x + 15],
+            ...tmp[x + 16],
+            ...tmp[x + 17]
+          },
+        ],
+
+        RoadMap: [
+          {
+            ...tmp[x + 18],
+            ...tmp[x + 19],
+            ...tmp[x + 20],
+            ...tmp[x + 21],
+          },
+        ],
+      };
+      break;
     }
-    Campus[sede_seleccionada.value]["integrantes"].unshift(object_union)
-    form_info_members.reset();
-    return (Swal.fire({
-        icon: 'success',
-        title: "Envio realizado",
-        text: 'Los datos se enviaron de forma exitosa'
-    }), console.log(Campus));
+  }
+  Campus["sedes"][sede_seleccionada.value]["integrantes"].unshift(object_union);
+  form_info_members.reset();
+  return (
+    Swal.fire({
+      icon: "success",
+      title: "Envio realizado",
+      text: "Los datos se enviaron de forma exitosa",
+    }),
+    console.log(Campus)
+  );
+};
 
-}
-
-form_sede.addEventListener("submit", e => addInfo(e));
+form_sede.addEventListener("submit", (e) => addInfo(e));
 form_info_members.addEventListener("submit", (e) => addPropertys(e));
-
